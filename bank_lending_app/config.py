3488de7e218c79b4631a61dca44fadd5e2f8ee5e
Harshitha Thangella <harshithathangella24@gmail.com>
2025-07-26 14:14:19 +0530

    Delete bank_lending_app directory

--- bank_lending_app/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-class Config:
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///bank.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- bank_lending_app/routes/ledger.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify, request
-from models import Loan, Payment
-from routes import ledger_bp
-
-@ledger_bp.route("/ledger/<int:loan_id>", methods=["GET"])
-def get_ledger(loan_id):
-    loan = Loan.query.get_or_404(loan_id)
-    ledger = [{"amount": p.amount, "date": p.payment_date.isoformat()} for p in loan.payments]
-    return jsonify({"loan_id": loan.id, "ledger": ledger})
